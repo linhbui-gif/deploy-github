890a37063e485b0555f9e4389c915e40c80d19e1
linh <buiquanglinh410@gmail.com>
2020-05-28 23:34:24 +0700

    Updates

---
+++ precache-manifest.a2bfdda887302cc04faa3b2b6f1b23e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d0b95a67f25570b95510ec157f71972",
+    "url": "/deploy-github/index.html"
+  },
+  {
+    "revision": "2025b19f54005d300709",
+    "url": "/deploy-github/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "70ca56a6072219061db5",
+    "url": "/deploy-github/static/js/2.7153c9fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-github/static/js/2.7153c9fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2025b19f54005d300709",
+    "url": "/deploy-github/static/js/main.713a92e6.chunk.js"
+  },
+  {
+    "revision": "1ea1789d7c12e740bc9c",
+    "url": "/deploy-github/static/js/runtime-main.f3b8de01.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-github/static/media/logo.5d5d9eef.svg"
+  }
+]);
